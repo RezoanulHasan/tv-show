d59c196d10da0157011ef34dbe27356f6ebf19bb
RezoanulHasan <rezoanulhasan96@gmail.com>
2024-02-01 18:57:15 +0600

    add footer

--- src/Component/Shared/Footer/Footer.jsx
+++ src/Component/Shared/Footer/Footer.jsx
@@ -1,11 +1,51 @@
-import React from 'react';
-
+import React from "react";
+import { Link } from "react-router-dom";
 const Footer = () => {
-    return (
-        <div>
-          <h1 className='text-4xl' >footer</h1>  
-        </div>
-    );
+  return (
+    <div className="mt-6">
+      <footer className="footer p-10 bg-base-200 text-base-content">
+        <nav>
+          <h6 className="footer-title">Shows</h6>
+          <a className="link link-hover">Now Playing</a>
+          <a className="link link-hover">Upcoming Shows</a>
+          <a className="link link-hover">Special Events</a>
+        </nav>
+        <nav>
+          <h6 className="footer-title">Bookings</h6>
+          <a src className="link link-hover">
+            Book Tickets
+          </a>
+          <Link to="/" className="link link-hover">
+            Book Tickets
+          </Link>
+          <Link to="/booking-history" className="link link-hover">
+            Booking History
+          </Link>
+        </nav>
+        <nav>
+          <h6 className="footer-title">Information</h6>
+          <a className="link link-hover">FAQs</a>
+          <a className="link link-hover">Venue Details</a>
+        </nav>
+        <form>
+          <h6 className="footer-title">Newsletter</h6>
+          <fieldset className="form-control w-80">
+            <label className="label">
+              <span className="label-text">Enter your email address</span>
+            </label>
+            <div className="join">
+              <input
+                type="text"
+                placeholder="username@site.com"
+                className="input input-bordered join-item"
+              />
+              <button className="btn btn-primary join-item">Subscribe</button>
+            </div>
+          </fieldset>
+        </form>
+      </footer>
+    </div>
+  );
 };
 
 export default Footer;
